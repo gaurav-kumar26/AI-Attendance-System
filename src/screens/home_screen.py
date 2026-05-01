10e65456ed9216b37b141f03075c08155e27172e
gaurav-kumar26 <gauravmodi1111@gmail.com>
2026-05-02 01:40:16 +0530

    Overwritting Streamlit Styles

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,10 +1,16 @@
 import streamlit as st
 from src.components.header import header_home
+from src.ui.base_layout import style_base_layout, style_background_home
 
 def home_screen():
-    st.header('Home Screen')
+    #st.header('Home Screen')
 
     header_home()
+
+    style_background_home()
+
+    style_base_layout()
+
     col1, col2 = st.columns(2)
 
     with col1:
